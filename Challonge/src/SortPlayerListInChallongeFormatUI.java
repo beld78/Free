d3c347d4662cc3d92f2c5198e9bae558ca128426
beld78 <belfami@gmail.com>
2016-06-05 21:13:30 +0200

    fix bug

--- Challonge/src/SortPlayerListInChallongeFormatUI.java
+++ Challonge/src/SortPlayerListInChallongeFormatUI.java
@@ -71,8 +71,8 @@ public class SortPlayerListInChallongeFormatUI {
 	}
 
 	private void writeParticipantsInGroups() {
-		for (int i = 0; i < amountOfGroups; i++) {
-			for (int z = 0; z < amountOfPersonsPerGroup; z++) {
+		for (int i = 0; i < amountOfPersonsPerGroup; i++) {
+			for (int z = 0; z < amountOfGroups; z++) {
 				groups.get(z)[i] = participants.get(participantPlace);
 				participantPlace++;
 			}
